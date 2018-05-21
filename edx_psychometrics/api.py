45888f254e9e4fb223173c3b8f31d3431340891e
MasterGowen <mastergowen@gmail.com>
2018-05-21 19:35:01 +0500

    absolute import task#7

--- edx_psychometrics/api.py
+++ edx_psychometrics/api.py
@@ -13,6 +13,7 @@ from lms.djangoapps.instructor.views.api import require_level, common_exceptions
 import edx_psychometrics
 
 from lms.djangoapps.instructor_task.api_helper import submit_task
+from lms import CELERY_APP
 
 log = logging.getLogger(__name__)
 
@@ -52,26 +53,29 @@ def get_psychometrics_data(request, course_id):
     return JsonResponse({"status": success_status})
 
 
-import logging
-from functools import partial
-
-from celery import task
-from django.conf import settings
-from django.utils.translation import ugettext_noop
-
-from lms.djangoapps.instructor_task.tasks_helper.runner import run_main_task
-from lms.djangoapps.instructor_task.tasks_base import BaseInstructorTask
-
-from .tasks_helper import PsychometricsReport
-
-TASK_LOG = logging.getLogger('edx.celery.task')
-
-
-@task(base=BaseInstructorTask, routing_key=settings.GRADES_DOWNLOAD_ROUTING_KEY)  # pylint: disable=not-callable
-def get_psychometrics_data_task(entry_id, xmodule_instance_args):
-    """
-    Generate psychometrics reports archive.
-    """
-    action_name = ugettext_noop('get_psychometrics_data')
-    task_fn = partial(PsychometricsReport.generate, xmodule_instance_args)
-    return run_main_task(entry_id, task_fn, action_name)
+@CELERY_APP.task
+def zip_student_submissions(course_id, block_id, locator_unicode, username):
+    return 1 + 1
+# import logging
+# from functools import partial
+#
+# from celery import task
+# from django.conf import settings
+# from django.utils.translation import ugettext_noop
+#
+# from lms.djangoapps.instructor_task.tasks_helper.runner import run_main_task
+# from lms.djangoapps.instructor_task.tasks_base import BaseInstructorTask
+#
+# from .tasks_helper import PsychometricsReport
+#
+# TASK_LOG = logging.getLogger('edx.celery.task')
+#
+#
+# @task(base=BaseInstructorTask, routing_key=settings.GRADES_DOWNLOAD_ROUTING_KEY)  # pylint: disable=not-callable
+# def get_psychometrics_data_task(entry_id, xmodule_instance_args):
+#     """
+#     Generate psychometrics reports archive.
+#     """
+#     action_name = ugettext_noop('get_psychometrics_data')
+#     task_fn = partial(PsychometricsReport.generate, xmodule_instance_args)
+#     return run_main_task(entry_id, task_fn, action_name)
