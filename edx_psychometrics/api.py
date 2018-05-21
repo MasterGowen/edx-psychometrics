ec3cac6e24ce56d8f4da48a3e936d2f260a009c2
MasterGowen <mastergowen@gmail.com>
2018-05-21 19:44:20 +0500

    absolute import task#9

--- edx_psychometrics/api.py
+++ edx_psychometrics/api.py
@@ -10,10 +10,12 @@ from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey, UsageKey
 from util.json_request import JsonResponse
 from lms.djangoapps.instructor.views.api import require_level, common_exceptions_400
-import edx_psychometrics
+from celery_utils.logged_task import LoggedTask
+from celery_utils.persist_on_failure import LoggedPersistOnFailureTask
 
 from lms.djangoapps.instructor_task.api_helper import submit_task
 from lms import CELERY_APP
+from celery import task
 
 log = logging.getLogger(__name__)
 
@@ -23,16 +25,21 @@ SUCCESS_MESSAGE_TEMPLATE = _("The {report_type} report is being created. "
                              "To view the status of the report, see Pending Tasks below.")
 
 
-def submit_get_psychometrics_data(request, course_key):
-    """
-    AlreadyRunningError is raised if an psychometrics report is already being generated.
-    """
-    task_type = 'get_psychometrics_data'
-    task_class = edx_psychometrics.api.get_psychometrics_data_task
-    task_input = {}
-    task_key = ''
+# def submit_get_psychometrics_data(request, course_key):
+#     """
+#     AlreadyRunningError is raised if an psychometrics report is already being generated.
+#     """
+#     task_type = 'get_psychometrics_data'
+#     task_class = edx_psychometrics.api.get_psychometrics_data_task
+#     task_input = {}
+#     task_key = ''
+#
+#     return submit_task(request, task_type, task_class, course_key, task_input, task_key)
 
-    return submit_task(request, task_type, task_class, course_key, task_input, task_key)
+@task(base=LoggedPersistOnFailureTask, bind=True)
+def update_course_schedules(self, **kwargs):
+    course_key = CourseKey.from_string(kwargs['course_key'])
+
 
 
 @transaction.non_atomic_requests
@@ -45,9 +52,9 @@ def get_psychometrics_data(request, course_id):
     """
     Pushes a Celery task which will aggregate psychometrics data.
     """
-    course_key = CourseKey.from_string(course_id)
+    #course_key = CourseKey.from_string(course_id)
     report_type = _('get_psychometrics_data')
-    submit_get_psychometrics_data(request, course_key)
+    update_course_schedules(request, course_id)
     success_status = SUCCESS_MESSAGE_TEMPLATE.format(report_type=report_type)
 
     return JsonResponse({"status": success_status})
