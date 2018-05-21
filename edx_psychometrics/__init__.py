28beb8d7f7b15ea305d8412f746ad7271db10797
MasterGowen <mastergowen@gmail.com>
2018-05-21 19:58:54 +0500

    absolute import task#11

--- edx_psychometrics/__init__.py
+++ edx_psychometrics/__init__.py
@@ -1,4 +1,8 @@
-from . import api, tasks, tasks_helper
+"""
+The psychometrics subsystem for the Open edX platform.
+"""
 
-__all__ = [api, tasks, tasks_helper]
+from . import api, tasks_helper, tasks
 
+__all__ = [api, tasks_helper, tasks]
+__version__ = '0.0.1'
