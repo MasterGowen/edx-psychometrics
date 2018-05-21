8970268ad7a2e6ab1da228990093c35cdf3b0d1b
MasterGowen <mastergowen@gmail.com>
2018-05-21 19:23:54 +0500

    absolute import task#6((

--- edx_psychometrics/api.py
+++ edx_psychometrics/api.py
@@ -27,7 +27,7 @@ def submit_get_psychometrics_data(request, course_key):
     AlreadyRunningError is raised if an psychometrics report is already being generated.
     """
     task_type = 'get_psychometrics_data'
-    task_class = edx_psychometrics.api.get_psychometrics_data
+    task_class = edx_psychometrics.api.get_psychometrics_data_task
     task_input = {}
     task_key = ''
 
@@ -50,3 +50,28 @@ def get_psychometrics_data(request, course_id):
     success_status = SUCCESS_MESSAGE_TEMPLATE.format(report_type=report_type)
 
     return JsonResponse({"status": success_status})
+
+
+import logging
+from functools import partial
+
+from celery import task
+from django.conf import settings
+from django.utils.translation import ugettext_noop
+
+from lms.djangoapps.instructor_task.tasks_helper.runner import run_main_task
+from lms.djangoapps.instructor_task.tasks_base import BaseInstructorTask
+
+from .tasks_helper import PsychometricsReport
+
+TASK_LOG = logging.getLogger('edx.celery.task')
+
+
+@task(base=BaseInstructorTask, routing_key=settings.GRADES_DOWNLOAD_ROUTING_KEY)  # pylint: disable=not-callable
+def get_psychometrics_data_task(entry_id, xmodule_instance_args):
+    """
+    Generate psychometrics reports archive.
+    """
+    action_name = ugettext_noop('get_psychometrics_data')
+    task_fn = partial(PsychometricsReport.generate, xmodule_instance_args)
+    return run_main_task(entry_id, task_fn, action_name)
