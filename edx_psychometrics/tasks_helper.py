924a8704d8974035a611a4137cf3000fa5722c71
MasterGowen <mastergowen@gmail.com>
2018-05-22 18:47:45 +0500

    _get_csv1_data#6

--- edx_psychometrics/tasks_helper.py
+++ edx_psychometrics/tasks_helper.py
@@ -8,6 +8,7 @@ from time import time
 from lazy import lazy
 from pytz import UTC
 from six import text_type
+import json
 
 from lms.djangoapps.instructor_analytics.csvs import format_dictlist
 
@@ -18,6 +19,7 @@ from opaque_keys.edx.keys import CourseKey, UsageKey
 from courseware.user_state_client import DjangoXBlockUserStateClient
 from lms.djangoapps.grades.course_grade_factory import CourseGradeFactory
 
+from lms.djangoapps.courseware.models import StudentModule
 from lms.djangoapps.instructor_task.tasks_helper.runner import TaskProgress
 from lms.djangoapps.instructor_task.tasks_helper.utils import upload_csv_to_report_store
 
@@ -75,21 +77,25 @@ class PsychometricsReport(object):
         course = get_course_by_id(course_id)
 
         rows = []
-        history_entries = []
-        for student, course_grade, error in CourseGradeFactory().iter(enrolled_students, course):
-            for location in cls._graded_scorable_blocks_to_header(course):
-                log.warning(str(location),
-                            "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                usage_key = UsageKey.from_string(location)
-                user_state_client = DjangoXBlockUserStateClient()
-                try:
-                    history_entries += list(user_state_client.get_history(student.username, usage_key))
-                except DjangoXBlockUserStateClient.DoesNotExist:
-                    pass
-        log.warning([str(e) for e in history_entries])
-        upload_csv_to_report_store(history_entries, csv_name, course_id, start_date)
 
-    @classmethod
+        for student, course_grade, error in CourseGradeFactory().iter(enrolled_students, course):
+            student_modules = StudentModule.objects.filter(
+                student=student,
+                course_id=course_id)
+            rows.append([s.state for s in student_modules])
+
+
+
+        #         usage_key = UsageKey.from_string(location)
+        #         user_state_client = DjangoXBlockUserStateClient()
+        #         try:
+        #             history_entries += list(user_state_client.get_history(student.username, usage_key))
+        #         except DjangoXBlockUserStateClient.DoesNotExist:
+        #             pass
+        # log.warning([str(e) for e in history_entries])
+        upload_csv_to_report_store(rows, csv_name, course_id, start_date)
+
+    @ classmethod
     def _graded_scorable_blocks_to_header(cls, course):
         """
         Returns an OrderedDict that maps a scorable block's id to its
