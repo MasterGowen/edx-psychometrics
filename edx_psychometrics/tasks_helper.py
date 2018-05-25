c8b625272b3dd7dd9155795975c47edcba91be62
vesloguzov <vesloguzov@gmail.com>
2018-05-25 13:14:21 +0500

    Course structure#15

--- edx_psychometrics/tasks_helper.py
+++ edx_psychometrics/tasks_helper.py
@@ -130,12 +130,12 @@ class PsychometricsReport(object):
         structure = CourseStructure.objects.get(course_id=course_id).ordered_blocks
         blocks = get_block_structure_manager(CourseKey.from_string(str(course_id))).get_collected()
         for block in blocks:
-            rows.append([type(block)], type(modulestore().get_item(block)))
+            rows.append([type(block), type(modulestore().get_item(block))])
         for block in blocks:
-        #     try:
-            rows.append([json.dumps(str(modulestore().get_item(block).get_state_for_lcp()))])
-            # except:
-            #     pass
+            try:
+                rows.append([json.dumps(str(modulestore().get_item(block).get_state_for_lcp()))])
+            except Exception as e:
+                rows.append([str(e)])
 
             # for name, field in block.fields.items():
             #     try:
