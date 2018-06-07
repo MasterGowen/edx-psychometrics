9ec0d09298079dc76223896467e9c35f26bd6ca1
vesloguzov <vesloguzov@gmail.com>
2018-06-07 21:23:48 +0500

    test

--- edx_psychometrics/tasks_helper.py
+++ edx_psychometrics/tasks_helper.py
@@ -139,26 +139,29 @@ class PsychometricsReport(object):
             if value['block_type'] == 'vertical':
                 for block in value['children']:
                     if structure[block]['block_type'] == 'problem':
-                        current_block = structure[block]
-                        usage_key = UsageKey.from_string(current_block['usage_key'])
-                        block = get_module_for_student(user, usage_key)
-                        state_inputs = block.displayable_items()[0].input_state.keys()
-                        loncapa_xml_tree = etree.XML(block.data)
-                        response_types = [node.tag for node in loncapa_xml_tree.iter() if
-                                          node.tag in registered_loncapa_tags]
-                        if len(state_inputs) > len(response_types):
-                            while len(state_inputs) != len(response_types):
-                                response_types.append(response_types[-1])
-                        for idx, input_state in enumerate(state_inputs):
-                            row = [
-                                input_state,
-                                response_types[idx],
-                                current_block['display_name'],
-                                key.split("@")[-1],
-                                module_order,
-                                value['display_name']
-                            ]
-                            datarows.append(row)
+                        try:
+                            current_block = structure[block]
+                            usage_key = UsageKey.from_string(current_block['usage_key'])
+                            block = get_module_for_student(user, usage_key)
+                            state_inputs = block.displayable_items()[0].input_state.keys()
+                            loncapa_xml_tree = etree.XML(block.data)
+                            response_types = [node.tag for node in loncapa_xml_tree.iter() if
+                                              node.tag in registered_loncapa_tags]
+                            if len(state_inputs) > len(response_types):
+                                while len(state_inputs) != len(response_types):
+                                    response_types.append(response_types[-1])
+                            for idx, input_state in enumerate(state_inputs):
+                                row = [
+                                    input_state,
+                                    response_types[idx],
+                                    current_block['display_name'],
+                                    key.split("@")[-1],
+                                    module_order,
+                                    value['display_name']
+                                ]
+                                datarows.append(row)
+                        except:
+                            pass
                     elif structure[block]['block_type'] == 'library_content':
                         for lib_item in structure[block]['children']:
                             current_block_lib = structure[lib_item]
